c59c3928e5634e16c1fb93989ec9f36b64df06fc
M2200781 <163543934+M2200781@users.noreply.github.com>
2024-10-29 20:31:24 -0300

    Update __init__.py

--- usuarios/__init__.py
+++ usuarios/__init__.py
@@ -3,11 +3,17 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+import os
 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///usuarios.db"
 app.config["SECRET_KEY"] = "07d53d8079db1adde65569a4fc8b467a"
+
+if os.getenv("DATABASE_URL"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///usuarios.db"
+
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
@@ -16,3 +22,4 @@ login_manager = LoginManager(app)
 login_manager.login_view = "index"
 
 from usuarios import routes
+
